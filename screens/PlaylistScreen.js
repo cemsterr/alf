3c2287396dd99a587b881dfd2eae12eef3421d7b
Cem Saglam <cemsaglam@sabanciuniv.edu>
2018-02-14 23:26:00 +0300

    Create basic screens

---
+++ screens/PlaylistScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text, Dimensions } from 'react-native';
+
+class PlaylistScreen extends Component {
+  render() {
+    return (
+      <View>        
+        <Text>PlaylistScreen</Text>
+        <Text>PlaylistScreen</Text>
+        <Text>PlaylistScreen</Text>
+        <Text>PlaylistScreen</Text>
+        <Text>PlaylistScreen</Text>
+        <Text>PlaylistScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default PlaylistScreen;
---
+++ screens/SettingsScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text, Dimensions } from 'react-native';
+
+class SettingsScreen extends Component {
+  render() {
+    return (
+      <View>        
+        <Text>SettingsScreen</Text>
+        <Text>SettingsScreen</Text>
+        <Text>SettingsScreen</Text>
+        <Text>SettingsScreen</Text>
+        <Text>SettingsScreen</Text>
+        <Text>SettingsScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default SettingsScreen;
